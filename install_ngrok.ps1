34a0cbd681e0908b9249dcf40dc20feb1317992c
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2023-11-18 15:19:19 +0100

    Update install_ngrok.ps1

--- install_ngrok.ps1
+++ install_ngrok.ps1
@@ -1,17 +1,3 @@
-# If user doesn't exist, create it
-if (!$userExists) {
-    # Create user runneradmin
-    $securePassword = ConvertTo-SecureString "Rabiu2004@" -AsPlainText -Force
-    New-LocalUser -Name "runneradmin" -Password $securePassword -FullName "Runner Admin" -Description "GitHub Actions Runner Admin"
-} else {
-    Write-Host "User runneradmin already exists."
-}
-
-# Enable Terminal Services
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 1
-
 # Download and extract ngrok
 Invoke-WebRequest -Uri https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
 Expand-Archive -Path ngrok.zip -DestinationPath .
