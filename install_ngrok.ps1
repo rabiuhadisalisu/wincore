ac89b3c35d7727ec53366bf24cf02cfac6b9290f
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2023-11-18 13:40:47 +0100

    Update install_ngrok.ps1

--- install_ngrok.ps1
+++ install_ngrok.ps1
@@ -1,8 +1,3 @@
-# install_ngrok.ps1
-param (
-    [string]$NGROK_AUTH_TOKEN
-)
-
 # Download and extract ngrok
 Invoke-WebRequest -Uri https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
 Expand-Archive -Path ngrok.zip -DestinationPath .
