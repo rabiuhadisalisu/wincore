b7e4f32f19cab8023c61fcb683acefee345573eb
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2024-05-11 15:38:18 +0100

    Update user.ps1

--- user.ps1
+++ user.ps1
@@ -1,16 +1,14 @@
 # Define variables
 $username = "rhsalisu"
 $fullname = "Rabiu Hadi Salisu"
-$password = "Rabiu2004@"
+$password = "Rhs2048@54200945"
+$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
 
 # Create the user account
-New-LocalUser -Name $username -FullName $fullname -Password (ConvertTo-SecureString -String $password -AsPlainText -Force) -PasswordNeverExpires:$true -UserMayNotChangePassword:$false
+New-LocalUser -Name $username -FullName $fullname -Password $securePassword -PasswordNeverExpires:$true -UserMayNotChangePassword:$false
 
 # Add the user to the local Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member $username
-
-# Set the password for the user
-Set-LocalUser -Name $username -Password (ConvertTo-SecureString -AsPlainText "Rabiu2004@" -Force) -Verbose
 
 # Enable Remote Desktop access for the user
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
