15272e9d960ceaa1f1ec521f076c7fd0b8c5175b
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2024-05-11 15:34:16 +0100

    Update user.ps1

--- user.ps1
+++ user.ps1
@@ -2,19 +2,20 @@
 $username = "rhsalisu"
 $fullname = "Rabiu Hadi Salisu"
 $password = "Rabiu2004@"
-$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
 
 # Create the user account
-New-LocalUser -Name $username -FullName $fullname -Password $securePassword -PasswordNeverExpires:$true -UserMayNotChangePassword:$false
+New-LocalUser -Name $username -FullName $fullname -Password (ConvertTo-SecureString -String $password -AsPlainText -Force) -PasswordNeverExpires:$true -UserMayNotChangePassword:$false
 
 # Add the user to the local Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member $username
+
+# Set the password for the user
+Set-LocalUser -Name $username -Password (ConvertTo-SecureString -AsPlainText "Rabiu2004@" -Force) -Verbose
 
 # Enable Remote Desktop access for the user
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 Set-LocalUser -Name $username -PasswordNeverExpires:$true
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 1
 
 # Disable new user welcome screen
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudExperienceHost\Welcome" -Name "SystemPaneSuggestionsEnabled" -Value 0
