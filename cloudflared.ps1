811d16f3fe3ec8297c8205cd1359521b02d92163
Rabiu Hadi Salisu <94430863+rabiuhadisalisu@users.noreply.github.com>
2024-11-21 16:33:39 +0100

    Update cloudflared.ps1

--- cloudflared.ps1
+++ cloudflared.ps1
@@ -11,7 +11,7 @@ if (-not (Test-Path -Path $env:TEMP)) {
 Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
 
 # Install Cloudflare Tunnel - Avoid hardcoding the token
-$token = $env:CLOUDFLARE_TUNNEL_TOKEN  # Access token from environment variable
+$token = eyJhIjoiMzk0M2Q0ZWMxOGM1MzkxZmJiZTkxNThhNWQ2MjliNTUiLCJ0IjoiZDZmYTU2YjMtODRhNy00MDc0LTljZjgtMmFlODQ5ODM1NzQ2IiwicyI6Ik9UUmxOMkppWWpVdE0yVTROUzAwT1dZekxXSmlOV010WkdJMU56bGpaVFJoTXpnMCJ9
 if (-not $token) {
   Write-Error "Error: CLOUDFLARE_TUNNEL_TOKEN environment variable not set."
   exit 1
