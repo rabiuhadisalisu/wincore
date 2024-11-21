ea80cc35821e9866e3d7a92500ef1ee23736df2f
Rabiu Hadi Salisu <94430863+rabiuhadisalisu@users.noreply.github.com>
2024-11-21 16:29:17 +0100

    Update cloudflared.ps1

--- cloudflared.ps1
+++ cloudflared.ps1
@@ -1,9 +1,23 @@
 # Download Cloudflare Tunnel
-Invoke-WebRequest -Uri "https://bin.cloudflared.com/latest/cloudflared-linux-amd64.zip" -OutFile cloudflared.zip
-Expand-Archive -Path cloudflared.zip
+$downloadUrl = "https://github.com/cloudflare/cloudflared/releases/download/2024.11.1/cloudflared-windows-amd64.exe"
+$downloadPath = "$env:TEMP\cloudflared.exe"
 
-# Install Cloudflare Tunnel
-./cloudflared service install --token "eyJhIjoiMzk0M2Q0ZWMxOGM1MzkxZmJiZTkxNThhNWQ2MjliNTUiLCJ0IjoiZDZmYTU2YjMtODRhNy00MDc0LTljZjgtMmFlODQ5ODM1NzQ2IiwicyI6Ik9UUmxOMkppWWpVdE0yVTROUzAwT1dZekxXSmlOV010WkdJMU56bGpaVFJoTXpnMCJ9"
+# Ensure temporary directory exists
+if (-not (Test-Path -Path $env:TEMP)) {
+  New-Item -ItemType Directory -Path $env:TEMP
+}
+
+# Download Cloudflare Tunnel
+Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
+
+# Install Cloudflare Tunnel - Avoid hardcoding the token
+$token = $env:CLOUDFLARE_TUNNEL_TOKEN  # Access token from environment variable
+if (-not $token) {
+  Write-Error "Error: CLOUDFLARE_TUNNEL_TOKEN environment variable not set."
+  exit 1
+}
+
+& $downloadPath service install --token $token
 
 # Create an RDP Tunnel (Replace with your desired configuration)
-./cloudflared tunnel rdp --url rdp://0.0.0.0:3389
+& $downloadPath tunnel rdp --url rdp://your_rdp_server_ip:3389
