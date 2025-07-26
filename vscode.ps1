63bc052170f2cb690be0fff4af60595890f3eaf9
Rabiu Hadi Salisu <94430863+rabiuhadisalisu@users.noreply.github.com>
2025-07-26 02:19:40 +0100

    Update vscode.ps1

--- vscode.ps1
+++ vscode.ps1
@@ -13,7 +13,12 @@ if (-Not (Test-Path $vsCodePath)) {
     $vsCodePath = "${env:ProgramFiles}\Microsoft VS Code\Code.exe"
 }
 
+# === CONFIG ===
+$javaHomePath = "C:\hostedtoolcache\windows\Java_Temurin-Hotspot_jdk\21.0.7-6.0\x64"
 
+# ===. Set JAVA_HOME System Environment Variable ===
+[Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHomePath, "Machine")
+Write-Host "JAVA_HOME set to: $([Environment]::GetEnvironmentVariable("JAVA_HOME", "Machine"))"
 
 # Cleanup
 Remove-Item $installerPath -Force
